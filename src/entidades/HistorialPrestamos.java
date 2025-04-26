48a1f3f3ee6ce869fbedf6ce08fc7dbd0507ddf6
Castro Reyes Arturo Joel <misthorn33@gmail.com>
2025-04-26 11:49:53 -0500

    clase historial prestamos 2.0 issue #1

--- src/entidades/HistorialPrestamos.java
+++ src/entidades/HistorialPrestamos.java
@@ -3,11 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package entidades;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author artur
  */
 public class HistorialPrestamos {
-    
+    private List<PrestamoBibliotecario> historial;
+
+    public HistorialPrestamos() {
+        historial = new ArrayList<>();
+    }
+
+    public void agregarPrestamo(PrestamoBibliotecario prestamo) {
+        historial.add(prestamo);
+    }
+
+    public List<PrestamoBibliotecario> getHistorial() {
+        return historial;
+    }
+
+    public void mostrarHistorial() {
+        if (historial.isEmpty()) {
+            System.out.println("No hay préstamos registrados en el historial.");
+        } else {
+            for (int i = 0; i < historial.size(); i++) {
+                System.out.println("Préstamo #" + (i + 1));
+                System.out.println(historial.get(i).toString());
+                System.out.println("-----------------------------------");
+            }
+        }
+    }
 }
